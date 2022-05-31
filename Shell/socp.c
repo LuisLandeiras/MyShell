44fed24f162438de77e273dda9a71a9995707621
Landeiras <landeirasluis@gmail.com>
2022-05-31 20:29:24 +0100

    Finished

--- Shell/socp.c
+++ Shell/socp.c
@@ -1,25 +1,5 @@
 #include "shell.h"
 
-/*void ioCopy(int IN, int OUT){
-    int n;
-    char buf[BUFFSIZE];
-    while ( ( n = read (IN, buf, BUFFSIZE)) > 0){
-       if (write (OUT, buf, n) != n)
-       perror("Erro de escrita!\n");
-    }
-    if (n < 0){
-        perror("Erro de leitura!\n");
-    }
-    fclose(IN);
-    fclose(OUT);
-}
-
-void copy(char *fonte, char *destino){
-    FILE *f1 = fopen(fonte, "rb");
-    FILE *f2 = fopen(destino, "wb");
-    ioCopy(f1, f2);
-}*/
-
 int ioCopy(int IN, int OUT){
     int n;
     char buf[128];
